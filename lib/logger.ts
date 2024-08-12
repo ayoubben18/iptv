d8acdc4bfb0498c0e7be7364ace780a168655202
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-08-12 01:22:39 +0100

    chore: Update logger module for better logging capabilities

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,25 +1,28 @@
 import pino from "pino";
-import { ServerEnv } from "./env-server";
-// import pretty from "pino-pretty";
+import pretty from "pino-pretty";
+// import { ServerEnv } from "./env-server";
+// // import pretty from "pino-pretty";
 
-const transport = pino.transport({
-  targets: [
-    {
-      target: "pino-pretty",
-      options: { destination: process.stdout.fd },
-    },
-    {
-      target: "@logtail/pino",
-      options: { sourceToken: ServerEnv.BETTER_STACK_TOKEN },
-    },
-  ],
-});
+// const transport = pino.transport({
+//   targets: [
+//     {
+//       target: "pino-pretty",
+//       options: { destination: process.stdout.fd },
+//     },
+//     {
+//       target: "@logtail/pino",
+//       options: { sourceToken: ServerEnv.BETTER_STACK_TOKEN },
+//     },
+//   ],
+// });
 
-const logger = pino(
-  {
-    level: "info",
-  },
-  transport,
-);
+// const logger = pino(
+//   {
+//     level: "info",
+//   },
+//   transport,
+// );
+
+const logger = pino(pretty());
 
 export default logger;
